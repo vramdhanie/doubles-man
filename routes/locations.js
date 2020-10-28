65c07ed01c844ec930322df85356f9869c6be82f
Vincent Ramdhanie <vramdhanie@gmail.com>
2020-10-27 22:18:31 -0400

    Add POST /locations

--- routes/locations.js
+++ routes/locations.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const { response } = require("../app");
 const router = express.Router();
+const bodyParser = express.urlencoded({ extended: true });
 
 /* Create an end point function for GET /locations */
 router.get("/", function (req, res) {
@@ -16,6 +18,10 @@ router.get("/", function (req, res) {
     });
 });
 
+router.get("/form", function (req, res) {
+  res.render("add_location_form");
+});
+
 router.get("/:id", function (req, res) {
   const { id } = req.params;
   const knex = req.app.get("db");
@@ -26,8 +32,24 @@ router.get("/:id", function (req, res) {
     .then((data) => {
       res.render("locationVendorList", { vendors: data });
     });
+});
 
-  // code to get a specific location
+router.post("/", bodyParser, function (req, res) {
+  // get the location name from the
+  const { location_name } = req.body;
+
+  // validate the location
+  // skipping validation for now
+
+  // insert new location into the database
+  const knex = req.app.get("db");
+  return knex
+    .returning("id")
+    .insert({ name: location_name })
+    .into("location")
+    .then((id) => {
+      res.render("add_location_success", { id: id, name: location_name });
+    });
 });
 
 module.exports = router;
